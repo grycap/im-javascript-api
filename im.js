273da2f60d8307194d581692d1e738269739db8b
micafer <micafer1@upv.es>
2020-10-06 13:24:06 +0200

    Improve code

--- im.js
+++ im.js
@@ -16,9 +16,10 @@
 
 class IMResponse {
 
-    constructor(ok, data) {
+    constructor(ok, data, message) {
       this.ok = ok;
       this.data = data;
+      this.message = message;
     }
 
 }
@@ -69,6 +70,142 @@ class IMAuthData {
 }
 
 
+class IMVirtualMachine {
+
+  constructor(client, infid, id) {
+    this.client = client;
+    this.radl = null;
+    this.state = null;
+
+    if (id===undefined) {
+      // infid in this case is the full id of the VM
+      this.fullid = infid;
+      this.id = infid.substring(infid.lastIndexOf('/') + 1);
+      var infidFull = infid.substring(0, infid.lastIndexOf('/') - 4);
+      this.infid = infidFull.substring(infidFull.lastIndexOf('/') + 1);
+    } else {
+      this.infid = infid;
+      this.id = id;
+      this.fullid = this.client.imUrl + '/infrastructures/' + this.infid + "/vms/" + this.id;
+    }
+  }
+
+  async getInfo() {
+    const headers = {'Accept': 'application/json',
+                    'Authorization': this.client.authData.formatAuthData()};
+    const response = await fetch(this.fullid, {headers: headers});
+    const output = await response.json();
+    if (response.ok) {
+      this.radl = output['radl'];
+      return new IMResponse(true, this.radl, null);
+    } else {
+      return new IMResponse(false, null, output['message']);
+    }
+  }
+
+  async getProperty(infID, vmID, property) {
+    const headers = {'Accept': 'application/json',
+                    'Authorization': this.authData.formatAuthData()};
+    const url = this.fullid + "/" + property;
+    const response = await fetch(url, {headers: headers});
+    return new IMResponse(response.ok, await response.json());
+  }
+
+  async getVMContMsg(infID, vmID) {
+    return this.getVMProperty(infID, vmID, "contmsg")
+  }
+}
+
+
+class IMInfrastructure {
+
+  constructor(client, id) {
+    this.client = client;
+    this.vms = [];
+    this.state = null;
+
+    if (id.startsWith('https://') || id.startsWith('http://')) {
+      this.fullid = id;
+      this.id = id.substring(id.lastIndexOf('/') + 1); 
+    } else {
+      this.id = id;
+      this.fullid = this.client.imUrl + '/infrastructures/' + this.id;
+    }
+  }
+
+  async getInfo() {
+    const headers = {'Accept': 'application/json',
+                    'Authorization': this.client.authData.formatAuthData()};
+    const response = await fetch(this.fullid, {headers: headers});
+    const output = await response.json();
+    if (response.ok) {
+      this.vms = [];
+      output['uri-list'].forEach(vmID => {
+        this.vms.push(new IMVirtualMachine(this.client, vmID['uri']));
+      });
+      return new IMResponse(true, this.vms, null);
+    } else {
+      return new IMResponse(false, null, output['message']);
+    }
+  }
+
+  async destroy() {
+    const headers = {'Accept': 'application/json',
+                     'Authorization': this.client.authData.formatAuthData()};
+    const response = await fetch(this.fullid, {
+      method: 'DELETE',
+      headers: headers
+    })
+    if (response.ok) {
+      const output = await response.text();
+      return new IMResponse(response.ok, await output, null);
+    } else {
+      const output = await response.json();
+      return new IMResponse(response.ok, null, output['message']);
+    }
+  }
+
+  async getProperty(infID, property) {
+    const headers = {'Accept': 'application/json',
+                    'Authorization': this.client.authData.formatAuthData()};
+    const url = this.fullid + "/" + property;
+    const response = await fetch(url, {headers: headers});
+    const output = await response.json();
+    if (response.ok) {
+      return new IMResponse(response.ok, await output, null);
+    } else {
+      return new IMResponse(response.ok, null, output['message']);
+    }
+  }
+
+  async getState(infID) {
+    const headers = {'Accept': 'application/json',
+                    'Authorization': this.client.authData.formatAuthData()};
+    const url = this.fullid + "/state";
+    const response = await fetch(url, {headers: headers});
+    const output = await response.json();
+    if (response.ok) {
+      this.state = output['state']['state'];
+      // also update VMs state
+      for (let key in output['state']['vm_states']) {
+        this.vms.forEach(vm => {
+          if (key == vm.id) {
+            vm.state = output['state']['vm_states'][key];
+          }
+        });
+      }
+      return new IMResponse(response.ok, await output, null);
+    } else {
+      return new IMResponse(response.ok, null, output['message']);
+    }
+  }
+
+  async getContMsg(infID) {
+    return this.getProperty(infID, "contmsg")
+  }
+}
+
+
 class IMClient {
 
     constructor(imUrl, authData) {
@@ -82,63 +219,47 @@ class IMClient {
     }
 
     async getInfrastructureList() {
-        const headers = {'Accept': 'application/json',
-                        'Authorization': this.authData.formatAuthData()};
-        const url = this.imUrl + '/infrastructures';
-        const response = await fetch(url, {headers: headers});
-        return new IMResponse(response.ok, await response.json());
-    }
-
-    async getInfrastructureInfo(infID) {
       const headers = {'Accept': 'application/json',
                       'Authorization': this.authData.formatAuthData()};
-      const url = this.imUrl + '/infrastructures/' + infID;
+      const url = this.imUrl + '/infrastructures';
       const response = await fetch(url, {headers: headers});
-      return new IMResponse(response.ok, await response.json());
+      const output = await response.json();
+      if (response.ok) {
+        var infList = [];
+        output['uri-list'].forEach(infID => {
+          infList.push(new IMInfrastructure(this, infID['uri']));
+        });
+        return new IMResponse(true, infList, null);
+      } else {
+        return new IMResponse(false, null, output['message']);
+      }
     }
 
-    async getVMInfo(infID, vmID) {
-        const headers = {'Accept': 'application/json',
-                        'Authorization': this.authData.formatAuthData()};
-        const url = this.imUrl + '/infrastructures/' + infID + "/vms/" + vmID;
-        const response = await fetch(url, {headers: headers});
-        return new IMResponse(response.ok, await response.json());
+    async createInfrastructure(template, type="radl") {
+      var contentType = "text/plain"
+      if (type == "tosca" || type == "yaml") {
+        contentType = "text/yaml";
+      } else if (type == "json") {
+        contentType = "application/json";
+      } else if (type != "radl") {
+        throw "Invalid type. Only radl, json, tosca or yaml are accepted.";
+      }
+      const headers = {'Accept': 'application/json',
+                        'Authorization': this.authData.formatAuthData(),
+                        'Content-Type': contentType};
+      const url = this.imUrl + '/infrastructures';
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: headers,
+        body: template
+      })
+      const output = await response.json();
+      if (response.ok) {
+        inf = new IMInfrastructure(this, output['uri'])
+        return new IMResponse(true, inf, null);
+      } else {
+        return new IMResponse(false, null, output['message']);
+      }
     }
-  
-    async destroyInfrastructure(infID) {
-        const headers = {'Accept': 'application/json',
-                         'Authorization': this.authData.formatAuthData()};
-        const url = this.imUrl + '/infrastructures/' + infID;
-        const response = await fetch(url, {
-          method: 'DELETE',
-          headers: headers
-        })
-        if (response.ok) {
-          return new IMResponse(response.ok, await response.text());
-        } else {
-          return new IMResponse(response.ok, await response.json());
-        }
-      }
-
-      async createInfrastructure(template, type="radl") {
-        var contentType = "text/plain"
-        if (type == "tosca" || type == "yaml") {
-          contentType = "text/yaml";
-        } else if (type == "json") {
-          contentType = "application/json";
-        } else if (type != "radl") {
-          throw "Invalid type. Only radl, json, tosca or yaml are accepted.";
-        }
-        const headers = {'Accept': 'application/json',
-                         'Authorization': this.authData.formatAuthData(),
-                         'Content-Type': contentType};
-        const url = this.imUrl + '/infrastructures';
-        const response = await fetch(url, {
-          method: 'POST',
-          headers: headers,
-          body: template
-        })
-        return new IMResponse(response.ok, await response.json());
-      }
 
 }
